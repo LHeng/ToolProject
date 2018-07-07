3c5f9e3b15f1779f242ab8b487983047cb81f930
liuheng <heng.liu@uurobot.cn>
2018-07-07 11:13:33 +0800

    更新

--- LHTool/ViewController.swift
+++ LHTool/ViewController.swift
@@ -15,46 +15,59 @@ class ViewController: UIViewController {
     }
 
     @IBAction func date(_ sender: Any) {
-//       let alert = LHPickerView.init(frame: self.view.bounds, type: .date)
-//        alert.alpha = 0
-//        UIApplication.shared.keyWindow?.addSubview(alert)
-//        alert.dateblock = {(date) in
-//            let select = date
-//            let dateFormater = DateFormatter()
-//            dateFormater.dateFormat = "yyyy-MM-dd"
-//            let result = dateFormater.string(from: select)
-//            print("---\(result)")
-//        }
-//        alert.showInView()
-        MBProgressHUD().showSuccess(success: "你好呀")
+       let alert = LHPickerView.init(frame: self.view.bounds, type: .date)
+        alert.alpha = 0
+        UIApplication.shared.keyWindow?.addSubview(alert)
+        alert.dateblock = {(date) in
+            let select = date
+            let dateFormater = DateFormatter()
+            dateFormater.dateFormat = "yyyy-MM-dd"
+            let result = dateFormater.string(from: select)
+            print("---\(result)")
+        }
+        alert.showInView()
     }
     @IBAction func sele(_ sender: Any) {
-//        let alert = LHPickerView.init(frame: self.view.bounds, type: .selector)
-//        alert.dataList = ["男","女"]
-//        alert.alpha = 0
-//        UIApplication.shared.keyWindow?.addSubview(alert)
-//        alert.myblock = {(str) in
-//            print("---\(str)")
-//        }
-//        alert.showInView()
-        MBProgressHUD().showError(error: "我错了")
+        let alert = LHPickerView.init(frame: self.view.bounds, type: .selector)
+        alert.dataList = ["男","女"]
+        alert.alpha = 0
+        UIApplication.shared.keyWindow?.addSubview(alert)
+        alert.myblock = {(str) in
+            print("---\(str)")
+        }
+        alert.showInView()
     }
     
     @IBAction func time(_ sender: Any) {
-//        let alert = LHPickerView.init(frame: self.view.bounds, type: .time)
-//        alert.alpha = 0
-//        UIApplication.shared.keyWindow?.addSubview(alert)
-//        alert.dateblock = {(date) in
-//            let select = date
-//            let dateFormater = DateFormatter()
-//            dateFormater.dateFormat = "HH:mm:ss"
-//            let result = dateFormater.string(from: select)
-//            print("---\(result)")        }
-//        alert.showInView()
-        let HUD =  MBProgressHUD().showMessage( message: "我错了",view: view)
+        let alert = LHPickerView.init(frame: self.view.bounds, type: .time)
+        alert.alpha = 0
+        UIApplication.shared.keyWindow?.addSubview(alert)
+        alert.dateblock = {(date) in
+            let select = date
+            let dateFormater = DateFormatter()
+            dateFormater.dateFormat = "HH:mm:ss"
+            let result = dateFormater.string(from: select)
+            print("---\(result)")        }
+        alert.showInView()
+   
+    }
+    @IBAction func showError(_ sender: Any) {
+        MBProgressHUD().showError(error: "失败了")
+    }
+    
+    @IBAction func showSuccess(_ sender: Any) {
+        MBProgressHUD().showSuccess(success: "成功了")
+    }
+    
+    @IBAction func showMessge(_ sender: Any) {
+        MBProgressHUD().show(text: "你该吃药了", icon: "", vc: view)
+    }
+    
+    @IBAction func showLoading(_ sender: Any) {
+        let HUD =  MBProgressHUD().showMessage( message: "加载中...",view: view)
         
         let delaQueue = DispatchQueue.init(label: "com.syc.nd", qos: .userInteractive)
-        let delayTime : DispatchTimeInterval = .seconds(5)
+        let delayTime : DispatchTimeInterval = .seconds(3)
         delaQueue.asyncAfter(deadline: .now() + delayTime) {
             DispatchQueue.main.async(execute: {
                 HUD?.hideHUDForView(vc: self.view)
