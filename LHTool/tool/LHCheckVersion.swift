539795ec24d88d7873a7f8e8fe8eca5366e1f88a
liuheng <heng.liu@uurobot.cn>
2018-11-30 16:12:37 +0800

    更新

--- LHTool/tool/LHCheckVersion.swift
+++ LHTool/tool/LHCheckVersion.swift
@@ -20,57 +20,60 @@ class LHCheckVersion: NSObject {
         //获取appstore上的最新版本号
         let appUrl = URL.init(string: "http://itunes.apple.com/lookup?id=" + appID)
         let appMsg = try? String.init(contentsOf: appUrl!, encoding: .utf8)
+        guard (appMsg != nil) else { return }
         let appMsgDict:NSDictionary = getDictFromString(jString: appMsg!)
-        let appResultsArray:NSArray = (appMsgDict["results"] as? NSArray)!
+        let appResultsArray:NSArray = appMsgDict["results"] as! NSArray
         let appResultsDict:NSDictionary = appResultsArray.lastObject as! NSDictionary
         let appStoreVersion:String = appResultsDict["version"] as! String
-        let appStoreVersion_Float:Float = Float(appStoreVersion)!
-        
         //获取当前手机安装使用的版本号
         let localVersion:String = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-        let localVersion_Float:Float = Float(localVersion)!
-        
-        //用户是否设置不再提示
-        let userDefaults = UserDefaults.standard
-        let res = userDefaults.bool(forKey: "NO_ALERt_AGAIN")
-        //appstore上的版本号大于本地版本号 - 说明有更新
-        if appStoreVersion_Float > localVersion_Float && !res {
-            let alertC = UIAlertController.init(title: "版本更新了", message: "是否前往更新", preferredStyle: .alert)
-            let yesAction = UIAlertAction.init(title: "去更新", style: .default, handler: { (handler) in
-                self.updateApp(appId: appID)
-            })
-            let noAction = UIAlertAction.init(title: "下次再说", style: .cancel, handler: nil)
-            let cancelAction = UIAlertAction.init(title: "不再提示", style: .default, handler: { (handler) in
-                self.noAlertAgain()
-            })
-            alertC.addAction(yesAction)
-            alertC.addAction(noAction)
-            alertC.addAction(cancelAction)
-            UIApplication.shared.keyWindow?.rootViewController?.present(alertC, animated: true, completion: nil)
-            }
+        self.compareVersion(localVersion: localVersion, storeVersion: appStoreVersion,appID: appID)
+
     }
     
     //去更新
-    func updateApp(appId:String) {
+    private func updateApp(appId:String) {
         let updateUrl:URL = URL.init(string: "http://itunes.apple.com/app/id" + appId)!
         UIApplication.shared.open(updateUrl, options: [:], completionHandler: nil)
     }
     
     //不再提示
-    func noAlertAgain() {
+    private func noAlertAgain() {
         let userDefaults = UserDefaults.standard
         userDefaults.set(true, forKey: "NO_ALERt_AGAIN")
         userDefaults.synchronize()
     }
     
     //JSONString转字典
-    func getDictFromString(jString:String) -> NSDictionary {
+    private func getDictFromString(jString:String) -> NSDictionary {
         let jsonData:Data = jString.data(using: .utf8)!
         let dict = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)
         if dict != nil {
             return dict as! NSDictionary
         }
         return NSDictionary()
+    }
+
+    private func compareVersion(localVersion: String, storeVersion: String, appID: String) {
+        //用户是否设置不再提示
+        let userDefaults = UserDefaults.standard
+        let res = userDefaults.bool(forKey: "NO_ALERt_AGAIN")
+        guard localVersion.compare(storeVersion) == ComparisonResult.orderedDescending,!res else {
+            return
+        }
+        //appstore上的版本号大于本地版本号 - 说明有更新
+        let alertC = UIAlertController.init(title: "版本更新了", message: "是否前往更新", preferredStyle: .alert)
+        let yesAction = UIAlertAction.init(title: "去更新", style: .default, handler: { (handler) in
+            self.updateApp(appId: appID)
+        })
+        let noAction = UIAlertAction.init(title: "下次再说", style: .cancel, handler: nil)
+        let cancelAction = UIAlertAction.init(title: "不再提示", style: .default, handler: { (handler) in
+            self.noAlertAgain()
+        })
+        alertC.addAction(yesAction)
+        alertC.addAction(noAction)
+        alertC.addAction(cancelAction)
+        UIApplication.shared.keyWindow?.rootViewController?.present(alertC, animated: true, completion: nil)
     }
 
 }
--- LHTool/tool/UIColor+Category.swift
+++ LHTool/tool/UIColor+Category.swift
@@ -18,49 +18,70 @@ extension UIColor {
         case karPurple
         case karBlack
         case karGray
-        case colorRBG
-        case hexStringToColor
-        func color(R : CGFloat, G : CGFloat, B : CGFloat,A : CGFloat,hexString: String)->UIColor {
+        func color()->UIColor {
             switch self {
             case .mainBGColor:
-                return UIColor.karcolor.hexStringToColor.color(R:1,G:1,B:1,A:1,hexString:"#9f9f9f")
+                return hexStringToColor(hexString: "F9F9F9")
             case .karRed:
-                return UIColor.init(red: 255.0 / 255.0, green: 80.0 / 255.0, blue: 95.0 / 255.0, alpha: A)
+                return UIColor.init(red: 255.0 / 255.0, green: 80.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
             case .karRedPress:
-                return UIColor.init(red: 215.0 / 255.0, green: 70.0 / 255.0, blue: 84.0 / 255.0, alpha: A)
+                return UIColor.init(red: 215.0 / 255.0, green: 70.0 / 255.0, blue: 84.0 / 255.0, alpha: 1.0)
             case .karPink:
-                return UIColor.init(red: 255.0 / 255.0, green: 200.0 / 255.0, blue: 207.0 / 255.0, alpha: A)
+                return UIColor.init(red: 255.0 / 255.0, green: 200.0 / 255.0, blue: 207.0 / 255.0, alpha: 1.0)
             case .karBule:
-                return UIColor.init(red: 0.0 / 255.0, green: 188.0 / 255.0, blue: 252.0 / 255.0, alpha: A)
+                return UIColor.init(red: 0.0 / 255.0, green: 188.0 / 255.0, blue: 252.0 / 255.0, alpha: 1.0)
             case .karPurple:
-                return UIColor.init(red: 255.0 / 255.0, green: 0.0 / 255.0, blue: 128.0 / 255.0, alpha: A)
+                return UIColor.init(red: 255.0 / 255.0, green: 0.0 / 255.0, blue: 128.0 / 255.0, alpha: 1.0)
             case .karBlack:
-                return UIColor.init(red: 32.0 / 255.0, green: 32.0 / 255.0, blue: 32.0 / 255.0, alpha: A)
+                return UIColor.init(red: 32.0 / 255.0, green: 32.0 / 255.0, blue: 32.0 / 255.0, alpha: 1.0)
             case .karGray:
-                return UIColor.init(red: 129.0 / 255.0, green: 129.0 / 255.0, blue: 129.0 / 255.0, alpha: A)
-            case .colorRBG:
-                 return UIColor.init(red: R / 255.0, green: G / 255.0, blue: B / 255.0, alpha: A)
-            case .hexStringToColor:
-                var cString: String = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
-                if cString.count < 6 {return UIColor.black}
-                let index = cString.index(cString.endIndex, offsetBy: -6)
-                if cString.hasPrefix("0X") {cString = String(cString.suffix(from: index))}
-                if cString.hasPrefix("#") {cString = String(cString.suffix(from: index))}
-                if cString.count != 6 {return UIColor.black}
-                var range: NSRange = NSMakeRange(0, 2)
-                let rString = (cString as NSString).substring(with: range)
-                range.location = 2
-                let gString = (cString as NSString).substring(with: range)
-                range.location = 4
-                let bString = (cString as NSString).substring(with: range)
-                var r: UInt32 = 0x0
-                var g: UInt32 = 0x0
-                var b: UInt32 = 0x0
-                Scanner.init(string: rString).scanHexInt32(&r)
-                Scanner.init(string: gString).scanHexInt32(&g)
-                Scanner.init(string: bString).scanHexInt32(&b)
-                return UIColor(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: CGFloat(1))
+                return UIColor.init(red: 129.0 / 255.0, green: 129.0 / 255.0, blue: 129.0 / 255.0, alpha: 1.0)
             }
         }
+    }
+    
+    class func karRGB(R : CGFloat, G : CGFloat, B : CGFloat,A : CGFloat)->UIColor{
+        return UIColor.init(red: R / 255.0, green: G / 255.0, blue: B / 255.0, alpha: A)
+    }
+    
+    class func hexStringToColor(hexString: String) -> UIColor{
+        
+        var cString: String = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if cString.count < 6 {return UIColor.black}
+        
+        let index = cString.index(cString.endIndex, offsetBy: -6)
+        
+        if cString.hasPrefix("0X") {cString = String(cString.suffix(from: index))}
+        
+        if cString.hasPrefix("#") {cString = String(cString.suffix(from: index))}
+        
+        if cString.count != 6 {return UIColor.black}
+        
+        var range: NSRange = NSMakeRange(0, 2)
+        
+        let rString = (cString as NSString).substring(with: range)
+        
+        range.location = 2
+        
+        let gString = (cString as NSString).substring(with: range)
+        
+        range.location = 4
+        
+        let bString = (cString as NSString).substring(with: range)
+        
+        var r: UInt32 = 0x0
+        
+        var g: UInt32 = 0x0
+        
+        var b: UInt32 = 0x0
+        
+        Scanner.init(string: rString).scanHexInt32(&r)
+        
+        Scanner.init(string: gString).scanHexInt32(&g)
+        
+        Scanner.init(string: bString).scanHexInt32(&b)
+        
+        return UIColor(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: CGFloat(1))
     }
 }
