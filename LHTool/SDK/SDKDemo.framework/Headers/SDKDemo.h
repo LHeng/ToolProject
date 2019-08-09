4eedfe7aaa93fbef8b0566a69d515c31b0ea95f2
liuheng <heng.liu@uurobot.cn>
2019-08-09 17:20:50 +0800

    更新

---
+++ LHTool/SDK/SDKDemo.framework/Headers/SDKDemo.h
@@ -0,0 +1,18 @@
+//
+//  SDKDemo.h
+//  SDKDemo
+//
+//  Created by 刘恒 on 2019/7/31.
+//  Copyright © 2019 刘恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SDKDemo.
+FOUNDATION_EXPORT double SDKDemoVersionNumber;
+
+//! Project version string for SDKDemo.
+FOUNDATION_EXPORT const unsigned char SDKDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKDemo/PublicHeader.h>
+#import <SDKDemo/SDKDemoViewController.h>
---
+++ LHTool/SDK/SDKDemo.framework/Headers/SDKDemoViewController.h
@@ -0,0 +1,17 @@
+//
+//  SDKDemoViewController.h
+//  SDKDemo
+//
+//  Created by 刘恒 on 2019/7/31.
+//  Copyright © 2019 刘恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SDKDemoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
